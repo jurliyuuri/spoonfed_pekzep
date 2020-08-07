7d9979ee8fe4238272ec8a9161024721cbf1dc6b
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2020-08-07 23:20:37 +0900

    Get things to work

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,28 @@ struct VocabRow {
     english_gloss: String,
 }
 
+#[derive(Debug, Clone)]
+struct Vocab {
+    pekzep_latin: String,
+    pekzep_hanzi: String,
+    parts_of_speech: String,
+    parts_of_speech_supplement: String,
+    english_gloss: String,
+}
+
+impl Vocab {
+    pub fn to_tab_separated(&self) -> String {
+        format!(
+            "{}\t{}\t{}\t{}\t{}",
+            self.pekzep_latin,
+            self.pekzep_hanzi,
+            self.parts_of_speech,
+            self.parts_of_speech_supplement,
+            self.english_gloss
+        )
+    }
+}
+
 #[derive(Ser, De, Debug, Clone)]
 struct MainRow {
     english: String,
@@ -27,27 +49,35 @@ struct MainRow {
     lineparine: String,
 }
 
+use std::collections::HashMap;
 use std::collections::HashSet;
 
-fn parse_vocabs() -> Result<Vec<VocabRow>, Box<dyn Error>> {
+fn parse_vocabs() -> Result<HashMap<String, Vocab>, Box<dyn Error>> {
     let f = File::open("raw/Spoonfed Pekzep - 語彙整理（超草案）.tsv")?;
     let f = BufReader::new(f);
-
-    let mut rows = vec![];
-    let mut detect_dup_in_key = HashSet::new();
+    let mut res = HashMap::new();
     for line in f.lines() {
         // to prevent double quotes from vanishing, I do not read with CSV parser
         let row: VocabRow =
             StringRecord::from(line.unwrap().split('\t').collect::<Vec<_>>()).deserialize(None)?;
-
-        if !row.key.is_empty() && !detect_dup_in_key.insert(row.key.clone()) {
-            // in HashSet::insert, if the set did have this value present, false is returned.
+        if !row.key.is_empty()
+            && res
+                .insert(
+                    row.key.clone(),
+                    Vocab {
+                        pekzep_latin: row.pekzep_latin,
+                        pekzep_hanzi: row.pekzep_hanzi,
+                        parts_of_speech: row.parts_of_speech,
+                        parts_of_speech_supplement: row.parts_of_speech_supplement,
+                        english_gloss: row.english_gloss,
+                    },
+                )
+                .is_some()
+        {
             panic!("duplicate key detected: {}", row.key);
         }
-
-        rows.push(row);
     }
-    Ok(rows)
+    Ok(res)
 }
 
 fn parse_spoonfed() -> Result<Vec<MainRow>, Box<dyn Error>> {
@@ -85,6 +115,7 @@ struct HelloTemplate<'a> {
     prev_link: &'a str,
     next_link: &'a str,
     audio_path: &'a str,
+    analysis: &'a str,
 }
 
 mod filters {
@@ -93,6 +124,14 @@ mod filters {
         v[0] = v[0].to_uppercase().nth(0).unwrap();
         let s2: String = v.into_iter().collect();
         Ok(s2)
+    }
+    pub fn linebreaksandtabs(s: &str) -> ::askama::Result<String> {
+        let s = s.to_string();
+        Ok(format!(
+            "<table border=1>\n\t<tr><td>{}</td></tr>\n<table>",
+            s.replace("\t", "</td><td>")
+                .replace("\n", "</td></tr>\n\t<tr><td>")
+        ))
     }
 }
 
@@ -136,9 +175,10 @@ fn link_url(prev: &Option<MainRow>) -> String {
 fn main() -> Result<(), Box<dyn Error>> {
     let spoonfed_rows = parse_spoonfed()?;
 
-    let vocab_rows = parse_vocabs()?;
+    let vocab = parse_vocabs()?;
 
-    let mut rows2: Vec<Option<MainRow>> = spoonfed_rows.clone().into_iter().map(|r| Some(r)).collect();
+    let mut rows2: Vec<Option<MainRow>> =
+        spoonfed_rows.clone().into_iter().map(|r| Some(r)).collect();
     rows2.push(None);
     rows2.insert(0, None);
 
@@ -150,6 +190,21 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
                 let mut file =
                     File::create(format!("docs/{}.html", encode_to_url(&this.pekzep_latin)))?;
+                let analysis = if this.decomposed.is_empty() {
+                    vec![]
+                } else {
+                    this
+                    .decomposed.split('.')
+                    .map(|a| {
+                        let key = a.to_string().replace("!", " // ").replace("#", " // ");
+                        let res = vocab.get(&key).expect(&format!(
+                            "Cannot find key {} in the vocab list, found while analyzing {} (len {})",
+                            key, this.decomposed, this.decomposed.len()
+                        ));
+                        res.to_tab_separated()
+                    })
+                    .collect::<Vec<_>>()
+                };
                 let hello = HelloTemplate {
                     english: &this.english,
                     chinese_pinyin: &this.chinese_pinyin,
@@ -159,6 +214,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                     prev_link: &link_url(prev),
                     next_link: &link_url(next),
                     audio_path: &encode_to_sound_path(&this.pekzep_latin),
+                    analysis: &analysis.join("\n"),
                 };
                 write!(file, "{}", hello.render().unwrap())?;
             }
