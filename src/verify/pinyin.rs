4e85d0905d7c2036561bf9c6e9ef098e118b2e57
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2021-06-29 03:40:11 +0900

    Finish matching pinyin with hanzi

--- src/verify/pinyin.rs
+++ src/verify/pinyin.rs
@@ -1,37 +1,48 @@
 pub fn check(hanzi_str: &str, pinyin_str: &str) {
-    use log::warn;
+    use log::{info, warn};
     use pinyin::ToPinyinMulti;
     use pinyin_parser::PinyinParser;
 
-    // first do a fast, naive check
-    let mut pinyins = vec![];
-    for multi in hanzi_str.to_pinyin_multi().flatten() {
-        let mut candidates = vec![];
-        for p in multi {
-            candidates.push(p.with_tone());
-            candidates.push(p.plain()); // to allow light tone
+    info!("parsing {:?}:", pinyin_str);
+
+    let mut hanzi_iter = hanzi_str.chars();
+    for pinyin in PinyinParser::strict(pinyin_str) {
+        let (hanzi, candidates) = loop {
+            let hanzi = hanzi_iter.next().expect("hanzi ran out");
+
+            if let Some(multi) = hanzi.to_pinyin_multi() {
+                let mut candidates = vec![];
+                for cand_pinyin in multi {
+                    candidates.push(cand_pinyin.with_tone());
+                    candidates.push(cand_pinyin.plain()); // to allow light tone
+                }
+                break (hanzi, candidates);
+            }
+        };
+
+        if pinyin.ends_with('r') && !["er", "ēr", "ér", "ěr", "èr"].contains(&&pinyin[..]) {
+            // Erhua. Get the next Chinese character and verify that it is 儿 or 兒
+            loop {
+                let expect_儿 = hanzi_iter.next().expect("hanzi ran out, expected 儿 or 兒");
+                if expect_儿.to_pinyin_multi().is_some() {
+                    if "儿兒".contains(expect_儿) {
+                        break;
+                    }
+                    panic!(
+                        "expected 儿 or 兒 because of the rhotic pinyin {}, but instead found a Chinese character {}",
+                        pinyin, expect_儿
+                    )
+                }
+            }
+        } else {
+            if candidates.contains(&&pinyin[..]) {
+                continue;
+            }
+
+            warn!(
+                "{} not found within candidates {:?} possible for the Chinese character {}. Encountered this while matching `{}` with `{}`.",
+                pinyin, candidates, hanzi, pinyin_str, hanzi_str
+            );
         }
-        pinyins.push(candidates);
-    }
-
-    println!("parsing {:?}:", pinyin_str);
-
-    let pinyins2 = PinyinParser::strict(pinyin_str)
-        .into_iter()
-        .collect::<Vec<_>>();
-
-    if pinyins2.len() != pinyins.len() {
-        warn!(
-            "length differs:\npinyins: {:?}\npinyins2: {:?}",
-            pinyins, pinyins2
-        );
-    }
-
-    for (i, p) in pinyins2.iter().enumerate() {
-        if pinyins[i].contains(&&p[..]) {
-            continue;
-        }
-
-        warn!("{} not found within candidates {:?}", p, pinyins[i]);
     }
 }
