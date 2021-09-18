991f048db890d26972cb826cce5b83cc27ab7da3
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2021-09-18 15:41:01 +0900

    Reset the folder after each use

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use spoonfed_pekzep::*;
 use std::error::Error;
+
+fn reset_folder(path: &str) -> Result<(), Box<dyn Error>> {
+    eprintln!("Resetting {}", path);
+    std::fs::remove_dir_all(path)?;
+    std::fs::create_dir(path)?;
+    Ok(())
+}
 fn main() -> Result<(), Box<dyn Error>> {
     use std::env;
     if env::var("RUST_LOG").is_err() {
@@ -7,27 +14,37 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     env_logger::init();
 
+    // At each run, reset the content of docs/char_img,
+    // because later in the script this folder is automatically filled
+    // by the images required to render the page.
+    reset_folder("docs/char_img")?;
+
+    // At each run, reset the content of docs/phrase and docs/vocab
+    // because they will be filled later in the script.
+    reset_folder("docs/phrase")?;
+    reset_folder("docs/vocab")?;
+
     let data_bundle = verify::DataBundle::new()?;
 
-    eprintln!("Generating phrase/");
+    eprintln!("Generating docs/phrase/");
     generate_phrases(&data_bundle)?;
 
-    eprintln!("Generating vocab/");
+    eprintln!("Generating docs/vocab/");
     generate_vocabs(&data_bundle)?;
 
-    eprintln!("Generating vocab_list_internal.html");
+    eprintln!("Generating docs/vocab_list_internal.html");
     generate_vocab_list_internal(&data_bundle)?;
 
-    eprintln!("Generating vocab_list.html");
+    eprintln!("Generating docs/vocab_list.html");
     generate_vocab_list(&data_bundle)?;
 
-    eprintln!("Generating index.html");
+    eprintln!("Generating docs/index.html");
     generate_index(&data_bundle)?;
 
-    eprintln!("Writing raw.tsv");
+    eprintln!("Writing docs/raw.tsv");
     write_condensed_csv()?;
 
-    eprintln!("Writing raw.js");
+    eprintln!("Writing docs/raw.js");
     write_condensed_js()?;
 
     Ok(())
