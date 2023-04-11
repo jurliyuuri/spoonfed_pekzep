7a5177d6ce07d06b1b2300aed7a82fe1a736554f
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2023-04-12 01:09:05 +0900

    Rename: `line_breaks_and_tabs` --> `convert_line_breaks_and_tabs_into_single_table`

--- src/askama_templates/filters.rs
+++ src/askama_templates/filters.rs
@@ -10,8 +10,8 @@ pub fn capitalize_first_char(s: &str) -> ::askama::Result<String> {
 #[allow(clippy::unnecessary_wraps)]
 /// # Errors
 /// Never fails. This function returns `Result` solely for the purpose of conforming to the askama's interface.
-pub fn line_breaks_and_tabs(s: &str) -> ::askama::Result<String> {
-    Ok(normalizer::line_breaks_and_tabs(s))
+pub fn convert_line_breaks_and_tabs_into_single_table(s: &str) -> ::askama::Result<String> {
+    Ok(normalizer::convert_line_breaks_and_tabs_into_single_table(s))
 }
 
 #[allow(clippy::unnecessary_wraps)]
